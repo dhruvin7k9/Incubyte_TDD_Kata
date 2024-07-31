68c641176cd8c1ad146773639e30a5291a2cc793
dhruvin7k9 <dhruvink79@gmail.com>
2024-07-31 17:13:23 +0530

    filterNumberString : generates string token

--- src/main/java/com/kata/StringCalculator.java
+++ src/main/java/com/kata/StringCalculator.java
@@ -3,25 +3,32 @@ package com.kata;
 public class StringCalculator 
 {
     private String findDelimiters(String numbers) {
-        // regex to split the string
         String delimiter = "[,";
-        // check if the string starts with a custom delimiter
         if (numbers.startsWith("//")) 
         {
             int indexOfNewLine = numbers.indexOf("\n");
-            // get the custom delimiter, //[ -> delimiter = [ as [ is special character in regex
             String[] delimiterStrings = numbers.substring(2, indexOfNewLine).split("\\[|\\]");
             for (String s : delimiterStrings) 
             {
                 if (!s.isEmpty())
                     delimiter += "|" + s;
             }
-
-            numbers = numbers.substring(indexOfNewLine + 1);
         }
         delimiter += "|\n]";
 
         return delimiter;
+    }
+
+    private String[] filterNumberString (String numbers, String delimiters) {
+        String[] numberStringTokens = {};
+
+        if (numbers.startsWith("//")) 
+        {
+            int indexOfNewLine = numbers.indexOf("\n");
+            numbers = numbers.substring(indexOfNewLine +1);
+        }
+        numberStringTokens = numbers.split(delimiters);
+        return numberStringTokens;
     }
 
     public int add(String numbers) 
@@ -32,16 +39,10 @@ public class StringCalculator
         } 
         else 
         {
-            String delimiter = findDelimiters(numbers);
+            String delimiters = findDelimiters(numbers);
+            String[] numbersArray = filterNumberString(numbers, delimiters);
+            
 
-            if (numbers.startsWith("//")) 
-            {
-                int indexOfNewLine = numbers.indexOf("\n");
-                numbers = numbers.substring(indexOfNewLine +1);
-            }
-
-            // processing the numbers with filtered input string
-            String[] numbersArray = numbers.split(delimiter);
             int sum = 0, i;
             String negativeNumbers = "";
             for (i = 0; i < numbersArray.length; i++) 
