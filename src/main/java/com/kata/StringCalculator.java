54a50214306122ac493b5c79e1535d943bf626ab
dhruvin7k9 <dhruvink79@gmail.com>
2024-07-31 16:29:04 +0530

    refactored add method

--- src/main/java/com/kata/StringCalculator.java
+++ src/main/java/com/kata/StringCalculator.java
@@ -2,6 +2,28 @@ package com.kata;
 
 public class StringCalculator 
 {
+    private String findDelimiters(String numbers) {
+        // regex to split the string
+        String delimiter = "[,";
+        // check if the string starts with a custom delimiter
+        if (numbers.startsWith("//")) 
+        {
+            int indexOfNewLine = numbers.indexOf("\n");
+            // get the custom delimiter, //[ -> delimiter = [ as [ is special character in regex
+            String[] delimiterStrings = numbers.substring(2, indexOfNewLine).split("\\[|\\]");
+            for (String s : delimiterStrings) 
+            {
+                if (!s.isEmpty())
+                    delimiter += "|" + s;
+            }
+
+            numbers = numbers.substring(indexOfNewLine + 1);
+        }
+        delimiter += "|\n]";
+
+        return delimiter;
+    }
+
     public int add(String numbers) 
     {
         if (numbers.isEmpty()) 
@@ -10,23 +32,13 @@ public class StringCalculator
         } 
         else 
         {
-            // regex to split the string
-            String delimiter = "[,";
-            // check if the string starts with a custom delimiter
+            String delimiter = findDelimiters(numbers);
+
             if (numbers.startsWith("//")) 
             {
                 int indexOfNewLine = numbers.indexOf("\n");
-                // get the custom delimiter, //[ -> delimiter = [ as [ is special character in regex
-                String[] delimiterStrings = numbers.substring(2, indexOfNewLine).split("\\[|\\]");
-                for (String s : delimiterStrings) 
-                {
-                    if (!s.isEmpty())
-                        delimiter += "|" + s;
-                }
-
-                numbers = numbers.substring(indexOfNewLine + 1);
+                numbers = numbers.substring(indexOfNewLine +1);
             }
-            delimiter += "|\n]";
 
             // processing the numbers with filtered input string
             String[] numbersArray = numbers.split(delimiter);
